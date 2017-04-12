872caa5dd5db6357af567dd456f853246f36495b
Your Name <serhiizem@gmail.com>
2017-04-12 10:24:13 +0300

    Angular2 basic maven setup

--- backend/src/main/java/com/phonecompany/model/UserRole.java
+++ backend/src/main/java/com/phonecompany/model/UserRole.java
@@ -1,9 +1,24 @@
 package com.phonecompany.model;
 
 /**
- *
+ * Defines user roles
  */
 public enum UserRole {
+    /* Can create users with such roles as: CLIENT, CSR, PMG */
+    ADMIN("ADMIN"),
+    /* Will post complaints or make requests to the system regarding
+    details of the service */
+    CLIENT("CLIENT"),
+    /**/
+    CSR("CSR"),
+    /**/
+    PMG("PMG");
+
+    private String title;
+
+    UserRole(String title) {
+        this.title = title;
+    }
 
 
 }
