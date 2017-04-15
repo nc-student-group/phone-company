27626955a6e9f238ce1e55655915a414ead99a45
Your Name <serhiizem@gmail.com>
2017-04-15 10:45:22 +0300

    Removing @EnableWebMvc beacause of default servlet handling exception

--- src/main/java/com/phonecompany/Application.java
+++ src/main/java/com/phonecompany/Application.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 public class Application extends WebMvcConfigurerAdapter {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/phonecompany/config/AppConfig.java
+++ src/main/java/com/phonecompany/config/AppConfig.java
@@ -1,17 +1,19 @@
 package com.phonecompany.config;
 
 import com.phonecompany.service.UserDetailServiceImpl;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.*;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.Filter;
+import javax.sql.DataSource;
 
 @Configuration
-@EnableWebMvc
+@ComponentScan(basePackages = { "com.phonecompany" })
 public class AppConfig {
 
     @Bean
