03594ddf8620dc8829138cdee48503c8419c79d3
Oksanka <kusanchik@gmail.com>
2017-04-16 04:05:16 +0300

    Rejected commit: Default

--- src/test/java/com/phonecompany/dao/UserDaoImplTest.java
+++ src/test/java/com/phonecompany/dao/UserDaoImplTest.java
@@ -6,7 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
 
 
 /**
@@ -16,6 +21,12 @@ public class UserDaoImplTest extends AbstractTest{
 
     @Autowired
     private UserDaoImpl userDao;
+
+    @Autowired
+    private AddressDaoImpl addressDao;
+
+    @Autowired
+    private RoleDaoImpl roleDao;
 
     @Before
     public void setUp(){
@@ -32,8 +43,6 @@ public class UserDaoImplTest extends AbstractTest{
         Assert.assertFalse("User id = null", user.getId() == null);
     }
 
-
-
     @Test
     public void findByUsername() throws Exception {
         List<User> userList = userDao.getAll();
@@ -47,6 +56,86 @@ public class UserDaoImplTest extends AbstractTest{
         User user = userDao.findByUsername(userList.get(0).getEmail());
         Assert.assertFalse("User list = null", userList == null);
         Assert.assertFalse("User = null", user == null);
+    }
+
+    @Test
+    public void update() throws Exception{
+        // update all
+        User user = new User();
+        Long testId = userDao.getAll().get(0).getId();
+        user.setId(testId);
+        user.setEmail("w@w");
+        user.setPassword("12345");
+        user.setFirstName("");
+        user.setSecondName("");
+        user.setLastName("");
+        user.setAddress(addressDao.getAll().get(0));
+        user.setPhone("");
+        user.setRole(roleDao.getAll().get(0));
+        User newUser = userDao.update(user);
+        Assert.assertNotNull("User id = null", user.getId());
+        Assert.assertEquals("User field \"Email\" is not change",
+                user.getEmail(), newUser.getEmail());
+        Assert.assertEquals("User field \"Password\" is not change",
+                user.getPassword(), newUser.getPassword());
+    }
+
+    @Test
+    public void getById() throws Exception {
+        Long testId = userDao.getAll().get(0).getId();
+        User user = userDao.getById(testId);
+        assertNotNull("User by id = null", user);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        User user = new User();
+        user.setEmail("User1");
+        user.setPassword("12345");
+        userDao.save(user);
+
+        Long testId = user.getId();
+        userDao.delete(testId);
+        user = userDao.getById(testId);
+        Assert.assertNull("User is not deleted", user);
+    }
+
+    @Test
+    public void getQuery() throws Exception {
+        Assert.assertNotNull("Query \"getById\" is not found", userDao.getQuery("getById"));
+        Assert.assertNotNull("Query \"save\" is not found", userDao.getQuery("save"));
+        Assert.assertNotNull("Query \"update\" is not found", userDao.getQuery("update"));
+        Assert.assertNotNull("Query \"delete\" is not found", userDao.getQuery("delete"));
+        Assert.assertNotNull("Query \"getAll\" is not found", userDao.getQuery("getAll"));
+    }
+
+    @Test
+    public void populateSaveStatement() throws Exception {
+
+    }
+
+    @Test
+    public void populateUpdateStatement() throws Exception {
+
+    }
+
+    @Test
+    public void init() throws Exception {
+        Map<Integer, Object> params = new HashMap<>();
+        String query = userDao.getQuery("getAll");
+        ResultSet rs = userDao.executeSelect(query, params);
+        if(!rs.isClosed()) {
+            User user = userDao.init(rs);
+            assertNotNull("User is not initialized", user);
+        }
+    }
+
+    @Test
+    public void executeSelect() throws Exception {
+        Map<Integer, Object> params = new HashMap<>();
+        String query = userDao.getQuery("getAll");
+        ResultSet rs = userDao.executeSelect(query, params);
+        Assert.assertFalse("Select is not execute (this ResultSet is closed)", rs.isClosed());
     }
 
 
---
+++ src/test/java/com/phonecompany/dao/VerificationTokenDaoImplTest.java
@@ -0,0 +1,134 @@
+package com.phonecompany.dao;
+
+import com.phonecompany.model.VerificationToken;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Oksanka on 15.04.2017.
+ */
+public class VerificationTokenDaoImplTest extends AbstractTest {
+    @Autowired
+    private VerificationTokenDaoImpl vtDao;
+
+    @Autowired
+    private UserDaoImpl userDao;
+
+    @Before
+    public void setUp(){
+        super.setUp();
+        vtDao.setAutoCommit(false);
+    }
+
+    @Test
+    public void save() throws Exception{
+        VerificationToken vt = new VerificationToken();
+        vt.setUser(userDao.getAll().get(8)); // userId in table verification-token id not duplicate
+        vt.setToken("token1");
+        vt.setExpireDate(new Date(0));
+        VerificationToken newVerificationToken = vtDao.save(vt);
+        assertNotNull("VerificationToken id = null", newVerificationToken.getId());
+        assertEquals("VerificationToken field \"User\" is not change",
+                vt.getUser(), newVerificationToken.getUser());
+        assertEquals("VerificationToken field \"Token\" is not change",
+                vt.getToken(), newVerificationToken.getToken());
+        assertEquals("VerificationToken field \"ExpireDate\" is not change",
+                vt.getExpireDate(), newVerificationToken.getExpireDate());
+    }
+
+    @Test
+    public void update() throws Exception{
+        VerificationToken vt = new VerificationToken();
+        Long testId = vtDao.getAll().get(8).getId();
+        vt.setId(testId);
+        vt.setUser(userDao.getAll().get(0));
+        vt.setToken("token1");
+        vt.setExpireDate(new Date(0));
+        VerificationToken newVerificationToken = vtDao.update(vt);
+        assertNotNull("VerificationToken id = null", vt.getId());
+        assertEquals("VerificationToken field \"User\" is not change",
+                vt.getUser(), newVerificationToken.getUser());
+        assertEquals("VerificationToken field \"Token\" is not change",
+                vt.getToken(), newVerificationToken.getToken());
+        assertEquals("VerificationToken field \"ExpireDate\" is not change",
+                vt.getExpireDate(), newVerificationToken.getExpireDate());
+    }
+
+    @Test
+    public void getById() throws Exception {
+        Long testId = vtDao.getAll().get(0).getId();
+        VerificationToken vt = vtDao.getById(testId);
+        assertNotNull("VerificationToken by id = null", vt);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        VerificationToken vt = new VerificationToken();
+        vt.setUser(userDao.getAll().get(0));
+        vt.setToken("token1");
+        vt.setExpireDate(new Date(0));
+        vt = vtDao.save(vt);
+
+        Long testId = vt.getId();
+        vtDao.delete(testId);
+        vt = vtDao.getById(testId);
+        Assert.assertNull("VerificationToken is not deleted", vt);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<VerificationToken> vts = vtDao.getAll();
+
+        assertNotNull("VerificationToken table is empty", vts);
+    }
+
+    @Test
+    public void getQuery() throws Exception {
+        assertNotNull("Query \"getById\" is not found", vtDao.getQuery("getById"));
+        assertNotNull("Query \"save\" is not found", vtDao.getQuery("save"));
+        assertNotNull("Query \"update\" is not found", vtDao.getQuery("update"));
+        assertNotNull("Query \"delete\" is not found", vtDao.getQuery("delete"));
+        assertNotNull("Query \"getAll\" is not found", vtDao.getQuery("getAll"));
+    }
+
+    @Test
+    public void populateSaveStatement() throws Exception {
+
+    }
+
+    @Test
+    public void populateUpdateStatement() throws Exception {
+
+    }
+
+    @Test
+    public void init() throws Exception {
+        Map<Integer, Object> params = new HashMap<>();
+        String query = vtDao.getQuery("getAll");
+        ResultSet rs = vtDao.executeSelect(query, params);
+        if(!rs.isClosed()) {
+            VerificationToken vt = vtDao.init(rs);
+            assertNotNull("VerificationToken is not initialized", vt);
+        }
+    }
+
+    @Test
+    public void executeSelect() throws Exception {
+        VerificationToken vt = new VerificationToken();
+        Map<Integer, Object> params = new HashMap<>();
+        String query = vtDao.getQuery("getAll");
+        ResultSet rs = vtDao.executeSelect(query, params);
+        Assert.assertFalse("Select is not execute (this ResultSet is closed)", rs.isClosed());
+    }
+}
