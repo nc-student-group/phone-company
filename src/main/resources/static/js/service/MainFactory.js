310e3a5fe1df8a85d793443e149783a656a8f877
ShpiliakYurii <shpilak.yura@gmail.com>
2017-04-17 10:12:32 +0300

    MainFactory host

---
+++ src/main/resources/static/js/service/MainFactory.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('phone-company').factory('MainFactory', [function () {
+    return {
+
+        host:"http://localhost:8090/",
+        // host:'https://phone-company.herokuapp.com',
+
+    }
+
+}]);
