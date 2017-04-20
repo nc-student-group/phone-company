7a3a8f1d5f07dfc2d20461b42b9838e5a8131b53
mrLordi <d.vodotiiets@yandex.ua>
2017-04-21 00:55:05 +0300

    entity User was created

--- src/main/java/com/phonecompany/model/User.java
+++ src/main/java/com/phonecompany/model/User.java
@@ -8,19 +8,32 @@ public class User extends DomainEntity {
     private String email;
     private String password;
     private UserRole role;
-    private Representative representative;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String phone;
+    private Address address;
+    private Corporate corporate;
+    private Boolean isRepresentative;
     private Status status;
 
     public User() {
     }
 
-    public User(String email, String password,
-                UserRole role, Representative representative,
-                Status status) {
+    public User(String email, String password, UserRole role,
+                String firstName, String secondName, String lastName,
+                String phone, Address address, Corporate corporate,
+                Boolean isRepresentative, Status status) {
         this.email = email;
         this.password = password;
         this.role = role;
-        this.representative = representative;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.address = address;
+        this.corporate = corporate;
+        this.isRepresentative = isRepresentative;
         this.status = status;
     }
 
@@ -48,12 +61,60 @@ public class User extends DomainEntity {
         this.role = role;
     }
 
-    public Representative getRepresentative() {
-        return representative;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setRepresentative(Representative representative) {
-        this.representative = representative;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Corporate getCorporate() {
+        return corporate;
+    }
+
+    public void setCorporate(Corporate corporate) {
+        this.corporate = corporate;
+    }
+
+    public Boolean getRepresentative() {
+        return isRepresentative;
+    }
+
+    public void setRepresentative(Boolean representative) {
+        isRepresentative = representative;
     }
 
     public Status getStatus() {
@@ -70,7 +131,14 @@ public class User extends DomainEntity {
                 "email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 ", role=" + role +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address=" + address +
+                ", corporate=" + corporate +
+                ", isRepresentative=" + isRepresentative +
                 ", status=" + status +
-                "} " + super.toString();
+                '}' + super.toString();
     }
 }
