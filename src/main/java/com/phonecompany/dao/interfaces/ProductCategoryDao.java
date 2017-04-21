3020127b0e6bfc83961ef2357d5dcb53892bf8fe
Oksanka <kusanchik@gmail.com>
2017-04-22 01:23:00 +0300

    Add ProductCategoryDaoImpl and ServiceDaoImpl

--- src/main/java/com/phonecompany/dao/interfaces/ProductCategoryDao.java
+++ src/main/java/com/phonecompany/dao/interfaces/ProductCategoryDao.java
@@ -1,7 +1,9 @@
 package com.phonecompany.dao.interfaces;
 
+import com.phonecompany.model.ProductCategory;
+
 /**
  * Created by Oksanka on 22.04.2017.
  */
-public interface ProductCategoryDao {
+public interface ProductCategoryDao  extends CrudDao<ProductCategory> {
 }
