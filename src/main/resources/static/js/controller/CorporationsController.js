4c7e704007f1f617ebe0575e9a74a922c9cd7c06
nik9str <nik9str@gmail.com>
2017-05-01 21:18:06 +0300

    Corporation paging and editing.

--- src/main/resources/static/js/controller/CorporationsController.js
+++ src/main/resources/static/js/controller/CorporationsController.js
@@ -9,29 +9,96 @@
     function CorporationsController($scope, $log, CorporationService, $rootScope) {
         console.log('This is CorporationsController');
 
+        $scope.page = 0;
+        $scope.size = 5;
+        $scope.partOfName = "";
+        $scope.editing=false;
         $scope.preloader.send = true;
-        CorporationService.getAllCorporation().then(function (data) {
-            $scope.corporations = data;
-                $scope.preloader.send = false;
-        }
+
+        CorporationService.getAllCorporation($scope.page, $scope.size,$scope.partOfName).then(function (data) {
+            $scope.corporations = data.corporates;
+            $scope.corporationsSelected = data.corporatesSelected;
+            $scope.preloader.send = false;
+        },function (errData) {
+            $scope.preloader.send = false;
+            }
         );
 
         $scope.corporation = {
             corporateName:''
+        };
+
+        $scope.getAll = function(){
+            console.info($scope.partOfName);
+            $scope.preloader.send = true;
+            CorporationService.getAllCorporation($scope.page, $scope.size, $scope.partOfName).then(function (data) {
+                $scope.corporations = data.corporates;
+                $scope.preloader.send = false;
+            })
         };
         $scope.createCorporation = function() {
             CorporationService.saveCorporation($scope.corporation)
                 .then(function (createdCorporation) {
                     toastr.success("Corporation created");
                     $log.debug("Created Corporation: ", createdCorporation);
-                    $scope.corporations = CorporationService.getAllCorporation();
+                    CorporationService.getAllCorporation($scope.page, $scope.size, $scope.partOfName).then(function (data) {
+                        $scope.corporations=data.corporates;
+                    });
+                }, function (error) {
+                    toastr.error(error.data.message);
+                    $log.error("Failed to save corporation", error);
+                });
+        };
+        $scope.saveCorporation = function() {
+            CorporationService.saveEditedCorporation($scope.editCorporation)
+                .then(function (createdCorporation) {
+                    toastr.success("Corporation created");
+                    $log.debug("Created Corporation: ", createdCorporation);
+                    $scope.corporations = CorporationService.getAllCorporation($scope.page, $scope.size, $scope.partOfName);
                 }, function (error) {
                     toastr.error(error.data.message);
                     $log.error("Failed to save corporation", error);
                 });
         };
 
-        $scope.corporations = CorporationService.getAllCorporation();
+        $scope.editClick = function (corporation) {
+            $scope.editing=true;
+            console.log($scope.editing);
+            $scope.editCorporation = corporation;
+        };
 
+        $scope.nextPage = function () {
+            if ($scope.inProgress == false && ($scope.page + 1) * $scope.size < $scope.corporationsSelected) {
+                $scope.inProgress = true;
+                $scope.page = $scope.page + 1;
+                $scope.preloader.send = true;
+                CustomerService.getAllCorporation($scope.page, $scope.size, $scope.partOfName).then(function (data) {
+                    $scope.corporations = data.corporates;
+                    $scope.corporationsSelected = data.corporatesSelected;
+                    $scope.preloader.send = false;
+                    $scope.inProgress = false;
+                }, function () {
+                    $scope.preloader.send = false;
+                    $scope.inProgress = false;
+                });
+            }
+        };
+
+        $scope.previousPage = function () {
+            if ($scope.page > 0 && $scope.inProgress == false) {
+                $scope.inProgress = true;
+                $scope.page = $scope.page - 1;
+                $scope.preloader.send = true;
+                CustomerService.getAllCorporation($scope.page, $scope.size, $scope.partOfName).then(function (data) {
+                    $scope.corporations = data.corporates;
+                    $scope.corporationsSelected = data.corporatesSelected;
+                    $scope.preloader.send = false;
+                    $scope.inProgress = false;
+                }, function () {
+                    $scope.preloader.send = false;
+                    $scope.inProgress = false;
+                });
+            }
+        };
     }
 }());
