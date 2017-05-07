d964444bbf887bf77bf04b282ec7e3d2a1825060
ShpiliakYurii <shpilak.yura@gmail.com>
2017-05-07 15:34:04 +0300

    spring security migration to xml file

--- src/main/java/com/phonecompany/Application.java
+++ src/main/java/com/phonecompany/Application.java
@@ -3,12 +3,14 @@ package com.phonecompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableAsync
 @SpringBootApplication
+@ImportResource("classpath:security.xml")
 public class Application  {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
