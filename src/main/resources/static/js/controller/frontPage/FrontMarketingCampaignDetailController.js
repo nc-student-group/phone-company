6c729fca3c99d96382cce12a56a8fd0e0e525c87
mrLordi <d.vodotiiets@yandex.ua>
2017-05-27 20:30:33 +0300

    Button 'more' for marketing campaign in frontPage

---
+++ src/main/resources/static/js/controller/frontPage/FrontMarketingCampaignDetailController.js
@@ -0,0 +1,26 @@
+'use strict';
+angular.module('phone-company').controller('FrontMarketingCampaignDetailController', [
+    '$scope',
+    '$rootScope',
+    '$location',
+    '$routeParams',
+    'MarketingCampaignService',
+    '$mdDialog',
+    function ($scope, $rootScope, $location, $routeParams, MarketingCampaignService, $mdDialog) {
+        console.log('This is FrontMarketingCampaignDetailController');
+
+        $scope.preloader.send = true;
+        MarketingCampaignService.getMarketingCampaignForCustomerById($routeParams['id']).then(function (data) {
+            $scope.campaign = data;
+            $scope.tariffId = data.tariffRegion.tariff.id;
+            $scope.preloader.send = false;
+
+        }, function () {
+            $scope.preloader.send = false;
+        });
+
+        $scope.backClick = function () {
+            $location.path("/frontPage");
+        };
+
+    }]);
