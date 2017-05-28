e7bf6ffb43921bcf57a24a5a94ab1835bd65e516
Oksanka <kusanchik@gmail.com>
2017-05-28 16:21:34 +0300

    Tariff plan activation prohibition for not representative customers

--- src/main/resources/static/js/controller/client/TariffDetailController.js
+++ src/main/resources/static/js/controller/client/TariffDetailController.js
@@ -5,8 +5,9 @@ angular.module('phone-company').controller('TariffDetailController', [
     '$location',
     '$routeParams',
     'TariffService',
+    'CustomerInfoService',
     '$mdDialog',
-    function ($scope, $rootScope, $location, $routeParams, TariffService, $mdDialog) {
+    function ($scope, $rootScope, $location, $routeParams, TariffService, CustomerInfoService, $mdDialog) {
         console.log('This is TariffDetailController');
         $scope.activePage = 'tariffs';
         console.log($routeParams['id']);
@@ -22,23 +23,29 @@ angular.module('phone-company').controller('TariffDetailController', [
         }, function () {
             $scope.preloader.send = false;
         });
+        CustomerInfoService.getCustomer().then(function (data) {
+            $scope.user = data;
+        }, function () {
+        });
 
         $scope.activateClick = function () {
-            $scope.preloader.send = true;
-            TariffService.getCurrentCustomerTariff().then(function (data) {
-                $scope.currentTariff = data;
-                if ($scope.currentTariff.tariff != undefined && $scope.currentTariff.tariff.id == $scope.tariff.id) {
-                    toastr.error("This tariff plan is already activated for you!", 'Error');
-                } else {
-                    $scope.showChangeTariffModalWindow($scope.currentTariff, $scope.tariff, $scope.preloader, 0, false, undefined);
-                }
-                $scope.preloader.send = false;
-            }, function (data) {
-                $scope.preloader.send = false;
-                if (data.message != undefined) {
-                    toastr.error(data.message, 'Error');
-                }
-            })
+            if ($scope.user.corporate == true && $scope.user.isRepresentative == true) {
+                $scope.preloader.send = true;
+                TariffService.getCurrentCustomerTariff().then(function (data) {
+                    $scope.currentTariff = data;
+                    if ($scope.currentTariff.tariff != undefined && $scope.currentTariff.tariff.id == $scope.tariff.id) {
+                        toastr.error("This tariff plan is already activated for you!", 'Error');
+                    } else {
+                        $scope.showChangeTariffModalWindow($scope.currentTariff, $scope.tariff, $scope.preloader, 0, false, undefined);
+                    }
+                    $scope.preloader.send = false;
+                }, function (data) {
+                    $scope.preloader.send = false;
+                    if (data.message != undefined) {
+                        toastr.error(data.message, 'Error');
+                    }
+                })
+            }
         };
 
 
