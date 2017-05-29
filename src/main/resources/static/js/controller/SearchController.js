a68e748662820724d57b74fd16124b2a96a52580
ShpiliakYurii <shpilak.yura@gmail.com>
2017-05-29 15:21:00 +0300

    search pagination fix 2

--- src/main/resources/static/js/controller/SearchController.js
+++ src/main/resources/static/js/controller/SearchController.js
@@ -4,27 +4,27 @@
     angular.module('phone-company')
         .controller('SearchController', SearchController);
 
-    SearchController.$inject = ['$scope', '$log', '$location','SearchService','TariffService','CorporationService'];
+    SearchController.$inject = ['$scope', '$log', '$location', 'SearchService', 'TariffService', 'CorporationService'];
 
-    function SearchController($scope, $log, $location,SearchService,TariffService,CorporationService) {
+    function SearchController($scope, $log, $location, SearchService, TariffService, CorporationService) {
         console.log('This is SearchController');
         $scope.activePage = 'search';
-        $scope.selectedCategory ="";
-        $scope.partOfEmail="";
-        $scope.selectedRole="0";
-        $scope.selectedUserStatus="ALL";
-        $scope.partOfPhone="";
-        $scope.selectedRegion=0;
-        $scope.selectedCorporate=0;
-        $scope.partOfSurname="";
-        $scope.complaintStatus="-";
-        $scope.complaintCategory="-";
-        $scope.tariffFor="-";
-        $scope.tariffOrServiceStatus="-";
-        $scope.partOfTariffName="";
-        $scope.partOfServiceName="";
-        $scope.lowerPrice=0;
-        $scope.upperPrice=1000;
+        $scope.selectedCategory = "";
+        $scope.partOfEmail = "";
+        $scope.selectedRole = "0";
+        $scope.selectedUserStatus = "ALL";
+        $scope.partOfPhone = "";
+        $scope.selectedRegion = 0;
+        $scope.selectedCorporate = 0;
+        $scope.partOfSurname = "";
+        $scope.complaintStatus = "-";
+        $scope.complaintCategory = "-";
+        $scope.tariffFor = "-";
+        $scope.tariffOrServiceStatus = "-";
+        $scope.partOfTariffName = "";
+        $scope.partOfServiceName = "";
+        $scope.lowerPrice = 0;
+        $scope.upperPrice = 1000;
         $scope.page = 0;
         $scope.size = 10;
         $scope.inProgress = false;
@@ -35,76 +35,77 @@
         };
 
         $scope.searchUpdate = function () {
-            if($scope.selectedCategory == "USERS"){
+            $scope.page = 0;
+            if ($scope.selectedCategory == "USERS") {
                 $scope.preloader.send = true;
-                SearchService.getForUserCategory($scope.page, $scope.size,$scope.partOfEmail,$scope.selectedRole,$scope.selectedUserStatus).then(
+                SearchService.getForUserCategory($scope.page, $scope.size, $scope.partOfEmail, $scope.selectedRole, $scope.selectedUserStatus).then(
                     function (data) {
-                        $scope.users=data.users;
+                        $scope.users = data.users;
                         $scope.preloader.send = false;
                         $scope.entitiesSelected = data.entitiesSelected;
                         $scope.inProgress = false;
                     },
-                    function(err){
+                    function (err) {
                         toastr.error("Error");
                         $scope.inProgress = false;
                         $scope.preloader.send = false;
                     }
                 );
-            } else if($scope.selectedCategory == "CUSTOMERS"){
+            } else if ($scope.selectedCategory == "CUSTOMERS") {
                 $scope.preloader.send = true;
-                SearchService.getForCustomerCategory($scope.page, $scope.size,$scope.partOfEmail,$scope.selectedUserStatus,$scope.selectedRegion,$scope.partOfPhone,$scope.selectedCorporate,$scope.partOfSurname).then(
+                SearchService.getForCustomerCategory($scope.page, $scope.size, $scope.partOfEmail, $scope.selectedUserStatus, $scope.selectedRegion, $scope.partOfPhone, $scope.selectedCorporate, $scope.partOfSurname).then(
                     function (data) {
-                        $scope.customers=data.customers;
+                        $scope.customers = data.customers;
                         $scope.preloader.send = false;
                         $scope.entitiesSelected = data.entitiesSelected;
                         $scope.inProgress = false;
                     },
-                    function(err){
+                    function (err) {
                         toastr.error("Error");
                         $scope.inProgress = false;
                         $scope.preloader.send = false;
                     }
                 );
-            }else if($scope.selectedCategory == "COMPLAINTS"){
+            } else if ($scope.selectedCategory == "COMPLAINTS") {
                 $scope.preloader.send = true;
-                SearchService.getForComplaintsCategory($scope.page, $scope.size,$scope.partOfEmail,$scope.complaintStatus,$scope.complaintCategory).then(
+                SearchService.getForComplaintsCategory($scope.page, $scope.size, $scope.partOfEmail, $scope.complaintStatus, $scope.complaintCategory).then(
                     function (data) {
-                        $scope.complaints=data.complaints;
+                        $scope.complaints = data.complaints;
                         $scope.preloader.send = false;
                         $scope.inProgress = false;
                         $scope.entitiesSelected = data.entitiesSelected;
                     },
-                    function(err){
+                    function (err) {
                         toastr.error("Error");
                         $scope.inProgress = false;
                         $scope.preloader.send = false;
                     }
                 );
-            }else if($scope.selectedCategory == "TARIFFS"){
+            } else if ($scope.selectedCategory == "TARIFFS") {
                 $scope.preloader.send = true;
-                SearchService.getForTariffCategory($scope.page, $scope.size,$scope.partOfTariffName,$scope.tariffOrServiceStatus,$scope.tariffFor).then(
+                SearchService.getForTariffCategory($scope.page, $scope.size, $scope.partOfTariffName, $scope.tariffOrServiceStatus, $scope.tariffFor).then(
                     function (data) {
-                        $scope.tariffs=data.tariffs;
+                        $scope.tariffs = data.tariffs;
                         $scope.preloader.send = false;
                         $scope.inProgress = false;
                         $scope.entitiesSelected = data.entitiesSelected;
                     },
-                    function(err){
+                    function (err) {
                         toastr.error("Error");
                         $scope.inProgress = false;
                         $scope.preloader.send = false;
                     }
                 );
-            }else if($scope.selectedCategory == "SERVICES"){
+            } else if ($scope.selectedCategory == "SERVICES") {
                 $scope.preloader.send = true;
-                SearchService.getForServiceCategory($scope.page, $scope.size,$scope.partOfServiceName,$scope.lowerPrice,$scope.upperPrice,$scope.tariffOrServiceStatus).then(
+                SearchService.getForServiceCategory($scope.page, $scope.size, $scope.partOfServiceName, $scope.lowerPrice, $scope.upperPrice, $scope.tariffOrServiceStatus).then(
                     function (data) {
-                        $scope.services=data.services;
+                        $scope.services = data.services;
                         $scope.entitiesSelected = data.entitiesSelected;
                         $scope.inProgress = false;
                         $scope.preloader.send = false;
                     },
-                    function(err){
+                    function (err) {
                         toastr.error("Error");
                         $scope.inProgress = false;
                         $scope.preloader.send = false;
